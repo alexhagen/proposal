f92a58772310e6042823855c5841fe4b101b2e09
Alex Hagen <ahagen@purdue.edu>
2015-12-02 01:12:22 -0500

    finished the book, thank goodness

--- john_muir_trail_day_split.py
+++ john_muir_trail_day_split.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("/home/ahagen/code");
+import matplotlib.pyplot as plt
 from pym import func as ahm
 from pyg import twod as ahp
 import numpy as np
@@ -14,12 +15,14 @@ elevation = arr[:,1];
 jmt = ahm.curve(mileage,elevation,name="John Muir Trail Elevation");
 
 # Plot this elevation to one line
-jmt_plot = jmt.plot(linecolor='#ff0000',linestyle='-');
-jmt_plot.lines_on();
-jmt_plot.markers_on();
-jmt_plot.xlabel('Distance ($s$) [$mi$]');
-jmt_plot.ylabel('Elevation ($z$) [$ft$]');
+jmt_plot = jmt.plot(linecolor='#ff0000', linestyle='-')
+jmt_plot.fill_between(mileage, np.zeros_like(mileage), elevation)
+jmt_plot.lines_on()
+jmt_plot.markers_on()
+jmt_plot.xlabel('Distance ($s$) [$mi$]')
+jmt_plot.ylabel('Elevation ($z$) [$ft$]')
+jmt_plot.xlim(0,np.max(mileage))
 # add in points of interest
 
 # export
-jmt_plot.export('john_muir_trail_plan',formats=['png','pgf'],sizes=['cs'],customsize=[7.4,2.]);
+jmt_plot.export('john_muir_trail_plan',formats=['png','pgf','websvg'],sizes=['cs'],customsize=[7.4,1.5]);
